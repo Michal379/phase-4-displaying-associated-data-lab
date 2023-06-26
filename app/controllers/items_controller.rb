c37b8d0aaa69e93bcde53297ccf8fcf5185e1932
Michal <khakaimic4@gmail.com>
2023-06-26 22:46:34 +0300

    completed lab

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
-    
     def index
-        items = Item.all
-        render json: items
+      items = Item.all
+      render json: items, include: { user: { only: [:id, :username, :city] } }
     end
-end
+  end
+  
